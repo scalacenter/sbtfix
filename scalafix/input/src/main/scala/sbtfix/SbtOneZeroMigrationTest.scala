219dea5f49ddee8bc4fcf45a24da078307cc62fa
jvican <jorgevc@fastmail.es>
2017-06-15 14:49:16 +0200

    Add tests for map and apply

--- scalafix/input/src/main/scala/sbtfix/SbtOneZeroMigrationTest.scala
+++ scalafix/input/src/main/scala/sbtfix/SbtOneZeroMigrationTest.scala
@@ -109,4 +109,24 @@ object SbtOneZeroMigrationTest {
       key4.value
     }
   }
+
+  object mapAndApply {
+    val sett1 = settingKey[String]("SettingKey 1")
+    val sett2 = settingKey[String]("SettingKey 2")
+    val sett3 = settingKey[String]("SettingKey 3")
+
+    val task1 = taskKey[String]("TaskKey 1")
+    val task2 = taskKey[String]("TaskKey 2")
+    val task3 = taskKey[String]("TaskKey 3")
+    val task4 = taskKey[String]("TaskKey 4")
+
+    sett1 := "s1"
+    sett2 := "s2"
+    sett3 <<= (sett1, sett2)(_ + _)
+
+    task1 := { println("t1"); "t1" }
+    task2 := { println("t2"); "t2" }
+    task3 <<= (task1, task2) map { (t1, t2) => println(t1 + t2); t1 + t2 }
+    task4 <<= (sett1, sett2) map { (s1, s2) => println(s1 + s2); s1 + s2 }
+  }
 }
